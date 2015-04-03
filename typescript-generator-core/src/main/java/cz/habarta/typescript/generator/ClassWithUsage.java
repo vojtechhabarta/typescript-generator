4daa4ff53a04fc6665d73c75f9472cb830ddb999
vojtechhabarta <vojtechhabarta@users.noreply.github.com>
2015-04-03 16:21:37 +0200

    added missing class ClassWithUsage

---
+++ typescript-generator-core/src/main/java/cz/habarta/typescript/generator/ClassWithUsage.java
@@ -0,0 +1,21 @@
+
+package cz.habarta.typescript.generator;
+
+
+public class ClassWithUsage {
+    
+    public final Class<?> beanClass;
+    public final String usedInProperty;
+    public final Class<?> usedInClass;
+
+    public ClassWithUsage(Class<?> beanClass, String usedInProperty, Class<?> usedInClass) {
+        this.beanClass = beanClass;
+        this.usedInProperty = usedInProperty;
+        this.usedInClass = usedInClass;
+    }
+
+    public String usage() {
+        return usedInClass.getSimpleName() + "." + usedInProperty;
+    }
+
+}
