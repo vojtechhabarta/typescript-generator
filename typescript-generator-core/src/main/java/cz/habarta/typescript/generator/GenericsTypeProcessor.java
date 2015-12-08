2c9cadccd868bf32c8ddb94aa9297d3a962b7d69
vojtechhabarta <vojtech@habarta.cz>
2015-12-08 14:41:11 +0100

    GenericsTypeProcessor extracted from test and made public

---
+++ typescript-generator-core/src/main/java/cz/habarta/typescript/generator/GenericsTypeProcessor.java
@@ -0,0 +1,75 @@
+
+package cz.habarta.typescript.generator;
+
+import java.lang.reflect.*;
+import java.util.*;
+
+
+public class GenericsTypeProcessor implements TypeProcessor {
+
+    @Override
+    public TypeProcessor.Result processType(Type javaType, TypeProcessor.Context context) {
+        if (javaType instanceof TypeVariable) {
+            final TypeVariable<?> typeVariable = (TypeVariable) javaType;
+            return new Result(new GenericVariableType(typeVariable.getName()));
+        }
+        if (javaType instanceof Class) {
+            final Class<?> javaClass = (Class<?>) javaType;
+            if (javaClass.getTypeParameters().length > 0) {
+                return processGenericClass(javaClass, javaClass.getTypeParameters(), context);
+            }
+        }
+        if (javaType instanceof ParameterizedType) {
+            final ParameterizedType parameterizedType = (ParameterizedType) javaType;
+            if (parameterizedType.getRawType() instanceof Class) {
+                final Class<?> javaClass = (Class<?>) parameterizedType.getRawType();
+                return processGenericClass(javaClass, parameterizedType.getActualTypeArguments(), context);
+            }
+        }
+        return null;
+    }
+
+    private Result processGenericClass(Class<?> rawType, Type[] typeArguments, TypeProcessor.Context context) {
+        if (!Collection.class.isAssignableFrom(rawType) && !Map.class.isAssignableFrom(rawType)) {
+            final List<Class<?>> discoveredClasses = new ArrayList<>();
+            // raw type
+            final String rawTsTypeName = context.getMappedName(rawType);
+            discoveredClasses.add(rawType);
+            // type arguments
+            final List<TsType> tsTypeArguments = new ArrayList<>();
+            for (Type typeArgument : typeArguments) {
+                final TypeProcessor.Result typeArgumentResult = context.processType(typeArgument);
+                tsTypeArguments.add(typeArgumentResult.getTsType());
+                discoveredClasses.addAll(typeArgumentResult.getDiscoveredClasses());
+            }
+            // result
+            final GenericStructuralType type = new GenericStructuralType(rawTsTypeName, tsTypeArguments);
+            return new Result(type, discoveredClasses);
+        }
+        return null;
+    }
+
+    private static class GenericStructuralType extends TsType.StructuralType {
+
+        public final List<TsType> typeArguments;
+
+        public GenericStructuralType(java.lang.String name, List<TsType> typeArguments) {
+            super(name);
+            this.typeArguments = typeArguments;
+        }
+
+        @Override
+        public java.lang.String toString() {
+            return name + "<" + ModelCompiler.join(typeArguments, ", ") + ">";
+        }
+    }
+    
+    private static class GenericVariableType extends TsType.BasicType {
+
+        public GenericVariableType(String name) {
+            super(name);
+        }
+
+    }
+
+}
--- typescript-generator-core/src/test/java/cz/habarta/typescript/generator/GenericsTest.java
+++ typescript-generator-core/src/test/java/cz/habarta/typescript/generator/GenericsTest.java
@@ -1,7 +1,6 @@
 package cz.habarta.typescript.generator;
 
 import java.io.*;
-import java.lang.reflect.*;
 import java.util.*;
 import static org.junit.Assert.*;
 import org.junit.*;
@@ -32,48 +31,7 @@ public class GenericsTest {
         final Settings settings = new Settings();
         settings.addTypeNamePrefix = "I";
         settings.noFileComment = true;
-        settings.customTypeProcessor = new TypeProcessor() {
-            @Override
-            public TypeProcessor.Result processType(Type javaType, TypeProcessor.Context context) {
-                if (javaType instanceof TypeVariable) {
-                    final TypeVariable<?> typeVariable = (TypeVariable) javaType;
-                    return new Result(new TsType.BasicType(typeVariable.getName()));
-                }
-                if (javaType instanceof Class) {
-                    final Class<?> javaClass = (Class<?>) javaType;
-                    if (javaClass.getTypeParameters().length > 0) {
-                        return processGenericClass(javaClass, false, javaClass.getTypeParameters(), context);
-                    }
-                }
-                if (javaType instanceof ParameterizedType) {
-                    final ParameterizedType parameterizedType = (ParameterizedType) javaType;
-                    if (parameterizedType.getRawType() instanceof Class) {
-                        final Class<?> javaClass = (Class<?>) parameterizedType.getRawType();
-                        return processGenericClass(javaClass, true, parameterizedType.getActualTypeArguments(), context);
-                    }
-                }
-                return null;
-            }
-            private Result processGenericClass(Class<?> rawType, boolean processRawType, Type[] typeArguments, TypeProcessor.Context context) {
-                if (!Collection.class.isAssignableFrom(rawType) && !Map.class.isAssignableFrom(rawType)) {
-                    final List<Class<?>> discoveredClasses = new ArrayList<>();
-                    // raw type
-                    final String rawTsTypeName = context.getMappedName(rawType);
-                    discoveredClasses.add(rawType);
-                    // type arguments
-                    final List<TsType> tsTypeArguments = new ArrayList<>();
-                    for (Type typeArgument : typeArguments) {
-                        final TypeProcessor.Result typeArgumentResult = context.processType(typeArgument);
-                        tsTypeArguments.add(typeArgumentResult.getTsType());
-                        discoveredClasses.addAll(typeArgumentResult.getDiscoveredClasses());
-                    }
-                    // result
-                    final GenericStructuralType type = new GenericStructuralType(rawTsTypeName, tsTypeArguments);
-                    return new Result(type, discoveredClasses);
-                }
-                return null;
-            }
-        };
+        settings.customTypeProcessor = new GenericsTypeProcessor();
 
         final StringWriter output = new StringWriter();
         new TypeScriptGenerator(settings).generateEmbeddableTypeScript(Arrays.asList(A.class), output, true, 0);
@@ -93,22 +51,6 @@ public class GenericsTest {
         assertEquals("IA<string, string>", compiler.typeFromJava(A.class.getField("x").getGenericType()).toString());
         assertEquals("IA<IA<string, IB>, string[]>", compiler.typeFromJava(A.class.getField("y").getGenericType()).toString());
         assertEquals("IA<{ [index: string]: V }, number[]>", compiler.typeFromJava(A.class.getField("z").getGenericType()).toString());
-    }
-
-    private static class GenericStructuralType extends TsType.StructuralType {
-
-        public final List<TsType> typeArguments;
-
-        public GenericStructuralType(String name, List<TsType> typeArguments) {
-            super(name);
-            this.typeArguments = typeArguments;
-        }
-
-        @Override
-        public java.lang.String toString() {
-            return name + "<" + ModelCompiler.join(typeArguments, ", ") + ">";
-        }
-
     }
 
     class A<U,V> {
