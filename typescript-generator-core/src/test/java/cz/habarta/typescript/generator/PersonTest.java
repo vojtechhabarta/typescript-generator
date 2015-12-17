141276c3a3cabb4da4fc628f65ff2f0c340aeb0c
vojtechhabarta <vojtech@habarta.cz>
2015-12-17 11:38:24 +0100

    fixed test

--- typescript-generator-core/src/test/java/cz/habarta/typescript/generator/PersonTest.java
+++ typescript-generator-core/src/test/java/cz/habarta/typescript/generator/PersonTest.java
@@ -2,7 +2,6 @@
 package cz.habarta.typescript.generator;
 
 import java.io.File;
-import java.util.Arrays;
 import org.junit.Test;
 
 
@@ -10,7 +9,7 @@ public class PersonTest {
     
     @Test
     public void test() {
-        new TypeScriptGenerator().generateTypeScript(Arrays.asList(Person.class), new File("target/person.d.ts"));
+        new TypeScriptGenerator().generateTypeScript(Input.from(Person.class), new File("target/person.d.ts"));
     }
 
 }
