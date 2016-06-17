b7786b2e2d84e91bb0253b3ee9ffdbea43e61db7
Vojtěch Habarta <vojtech@habarta.cz>
2016-06-17 13:35:05 +0200

    added test for generated interface order

--- typescript-generator-core/src/test/java/cz/habarta/typescript/generator/Jackson2PolymorphismTest.java
+++ typescript-generator-core/src/test/java/cz/habarta/typescript/generator/Jackson2PolymorphismTest.java
@@ -1,6 +1,7 @@
 
 package cz.habarta.typescript.generator;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,13 +10,36 @@ import org.junit.Test;
 public class Jackson2PolymorphismTest {
 
     @Test
-    public void test() {
+    public void testPropertyNameQuoting() {
         final String output = new TypeScriptGenerator(TestUtils.settings()).generateTypeScript(Input.from(BadFieldClass.class));
         Assert.assertTrue(output.contains("\"@class\""));
     }
 
     @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
     public static interface BadFieldClass {
+    }
+
+    @Test
+    public void testInterfaceOrder() {
+        final String output = new TypeScriptGenerator(TestUtils.settings()).generateTypeScript(Input.from(Z.class));
+        final int x = output.indexOf("interface X");
+        final int y = output.indexOf("interface Y");
+        final int z = output.indexOf("interface Z");
+        Assert.assertTrue(z < x);
+        Assert.assertTrue(z < y);
+    }
+
+    @JsonSubTypes({
+            @JsonSubTypes.Type(Y.class),
+            @JsonSubTypes.Type(X.class)
+    })
+    public static class Z {
+    }
+
+    public static class Y extends Z {
+    }
+
+    public static class X extends Z {
     }
 
 }
