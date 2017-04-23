68de0400da1f03431030651bd9781b351a1b7511
Vojtěch Habarta <vojtech@habarta.cz>
2017-04-23 23:06:57 +0200

    Fixed 'declare' modifier (refs #136)

--- typescript-generator-core/src/test/java/cz/habarta/typescript/generator/ModulesAndNamespacesTest.java
+++ typescript-generator-core/src/test/java/cz/habarta/typescript/generator/ModulesAndNamespacesTest.java
@@ -13,32 +13,40 @@ public class ModulesAndNamespacesTest {
 
     @Test
     public void testNamespacesAndModules() {
-        final File outputDir = new File("target/test-ts-modules");
-        outputDir.mkdirs();
-
-        file("Test1", null, null, TypeScriptOutputKind.global, TypeScriptFileType.declarationFile, new File(outputDir, "test-mn1.d.ts"));
-        file("Test2", null, "NS2", TypeScriptOutputKind.global, TypeScriptFileType.declarationFile, new File(outputDir, "test-mn2.d.ts"));
-        file("Test3a", "mod3a", null, TypeScriptOutputKind.ambientModule, TypeScriptFileType.declarationFile, new File(outputDir, "test-mn3a.d.ts"));
-        file("Test3b", null, null, TypeScriptOutputKind.module, TypeScriptFileType.declarationFile, new File(outputDir, "test-mn3b.d.ts"));
-        file("Test4a", "mod4a", "NS4a", TypeScriptOutputKind.ambientModule, TypeScriptFileType.declarationFile, new File(outputDir, "test-mn4a.d.ts"));
-        file("Test4b", null, "NS4b", TypeScriptOutputKind.module, TypeScriptFileType.declarationFile, new File(outputDir, "test-mn4b.d.ts"));
-
-        file("Test5", null, null, TypeScriptOutputKind.global, TypeScriptFileType.implementationFile, new File(outputDir, "test-mn5.ts"));
-        file("Test6", null, "NS6", TypeScriptOutputKind.global, TypeScriptFileType.implementationFile, new File(outputDir, "test-mn6.ts"));
-        file("Test7", null, null, TypeScriptOutputKind.module, TypeScriptFileType.implementationFile, new File(outputDir, "test-mn7.ts"));
-        file("Test8", null, "NS8", TypeScriptOutputKind.module, TypeScriptFileType.implementationFile, new File(outputDir, "test-mn8.ts"));
+        files(new File("target/test-ts-modules"), false);
+        files(new File("target/test-ts-modules-pkg"), true);
     }
 
-    private static void file(String prefix, String module, String namespace, TypeScriptOutputKind outputKind, TypeScriptFileType outputFileType, File output) {
+    public void files(File outputDir, boolean mapPackages) {
+        outputDir.mkdirs();
+
+        file("Test1", null, null, mapPackages, TypeScriptOutputKind.global, TypeScriptFileType.declarationFile, new File(outputDir, "test-mn1.d.ts"));
+        file("Test2", null, "NS2", mapPackages, TypeScriptOutputKind.global, TypeScriptFileType.declarationFile, new File(outputDir, "test-mn2.d.ts"));
+        file("Test3a", "mod3a", null, mapPackages, TypeScriptOutputKind.ambientModule, TypeScriptFileType.declarationFile, new File(outputDir, "test-mn3a.d.ts"));
+        file("Test3b", null, null, mapPackages, TypeScriptOutputKind.module, TypeScriptFileType.declarationFile, new File(outputDir, "test-mn3b.d.ts"));
+        file("Test4a", "mod4a", "NS4a", mapPackages, TypeScriptOutputKind.ambientModule, TypeScriptFileType.declarationFile, new File(outputDir, "test-mn4a.d.ts"));
+        file("Test4b", null, "NS4b", mapPackages, TypeScriptOutputKind.module, TypeScriptFileType.declarationFile, new File(outputDir, "test-mn4b.d.ts"));
+
+        file("Test5", null, null, mapPackages, TypeScriptOutputKind.global, TypeScriptFileType.implementationFile, new File(outputDir, "test-mn5.ts"));
+        file("Test6", null, "NS6", mapPackages, TypeScriptOutputKind.global, TypeScriptFileType.implementationFile, new File(outputDir, "test-mn6.ts"));
+        file("Test7", null, null, mapPackages, TypeScriptOutputKind.module, TypeScriptFileType.implementationFile, new File(outputDir, "test-mn7.ts"));
+        file("Test8", null, "NS8", mapPackages, TypeScriptOutputKind.module, TypeScriptFileType.implementationFile, new File(outputDir, "test-mn8.ts"));
+    }
+
+    private static void file(String prefix, String module, String namespace, boolean mapPackagesToNamespaces, TypeScriptOutputKind outputKind, TypeScriptFileType outputFileType, File output) {
         final Settings settings = new Settings();
         settings.jsonLibrary = JsonLibrary.jackson2;
         settings.addTypeNamePrefix = prefix;
         settings.module = module;
         settings.namespace = namespace;
+        settings.mapPackagesToNamespaces = mapPackagesToNamespaces;
         settings.outputKind = outputKind;
         settings.outputFileType = outputFileType;
         if (outputFileType == TypeScriptFileType.implementationFile) {
-            settings.extensions.add(new TestFunctionExtention());
+            settings.mapClasses = ClassMapping.asClasses;
+        }
+        if (outputFileType == TypeScriptFileType.implementationFile && !mapPackagesToNamespaces) {
+            settings.extensions.add(new TestFunctionExtension());
         }
         new TypeScriptGenerator(settings).generateTypeScript(Input.from(Data.class, Direction.class, Align.class), Output.to(output));
     }
@@ -59,12 +67,13 @@ public class ModulesAndNamespacesTest {
         Right
     }
 
-    private static class TestFunctionExtention extends EmitterExtension {
+    private static class TestFunctionExtension extends EmitterExtension {
 
         @Override
         public EmitterExtensionFeatures getFeatures() {
             final EmitterExtensionFeatures features = new EmitterExtensionFeatures();
             features.generatesRuntimeCode = true;
+            features.worksWithPackagesMappedToNamespaces = true;
             return features;
         }
 
