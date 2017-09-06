75907237e12a8547243230adf18dd6df022c01b4
Vojtěch Habarta <vojtech@habarta.cz>
2017-09-06 17:46:02 +0200

    JAXB sample

--- sample-maven/src/main/java/cz/habarta/typescript/generator/sample/Person.java
+++ sample-maven/src/main/java/cz/habarta/typescript/generator/sample/Person.java
@@ -2,17 +2,19 @@
 package cz.habarta.typescript.generator.sample;
 
 import java.util.*;
+import javax.xml.bind.annotation.*;
 
 
 public class Person {
 
     public String name;
     public int age;
+    @XmlElement(name = "has-children", required = true)
     public boolean hasChildren;
     public List<String> tags;
     public Map<String, String> emails;
 
-    @javax.persistence.Transient
+    @XmlTransient
     public String excluded;
 
 }
