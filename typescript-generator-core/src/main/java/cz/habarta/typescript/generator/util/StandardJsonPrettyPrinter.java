7d999730b1ac27ee474ee1c0a8cd943aa3cfe652
Vojtěch Habarta <vojtech@habarta.cz>
2018-01-24 16:34:58 +0100

    StandardJsonPrettyPrinter - empty objects as `{}` (without space)

--- typescript-generator-core/src/main/java/cz/habarta/typescript/generator/util/StandardJsonPrettyPrinter.java
+++ typescript-generator-core/src/main/java/cz/habarta/typescript/generator/util/StandardJsonPrettyPrinter.java
@@ -39,6 +39,17 @@ public class StandardJsonPrettyPrinter extends DefaultPrettyPrinter {
     }
 
     @Override
+    public void writeEndObject(JsonGenerator g, int nrOfEntries) throws IOException {
+        if (!_objectIndenter.isInline()) {
+            --_nesting;
+        }
+        if (nrOfEntries > 0) {
+            _objectIndenter.writeIndentation(g, _nesting);
+        }
+        g.writeRaw('}');
+    }
+
+    @Override
     public void writeEndArray(JsonGenerator g, int nrOfValues) throws IOException {
         if (!_arrayIndenter.isInline()) {
             --_nesting;
