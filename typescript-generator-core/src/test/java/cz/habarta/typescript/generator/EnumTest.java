6bf3790e1a165828766d008ea774dc5ab44bb63f
Jeramy Toplak <j.toplak@gmx.com>
2018-09-22 12:58:19 -0600

    Class enum compatibility with regular enums (#270)
    * handle enums with 'classEnumPattern'
    
    * ** doesn't work when path contains \
    
    * IntelliJ auto-import breaks AppVeyor check style :(
    
    * wrong import order

--- typescript-generator-core/src/test/java/cz/habarta/typescript/generator/EnumTest.java
+++ typescript-generator-core/src/test/java/cz/habarta/typescript/generator/EnumTest.java
@@ -9,7 +9,6 @@ import java.util.*;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-
 public class EnumTest {
 
     @Test
@@ -95,21 +94,26 @@ public class EnumTest {
     }
 
     @Test
-    public void testClassAsEnum() {
+    public void testEnumsWithClassEnumPattern() {
         final Settings settings = TestUtils.settings();
         settings.mapEnum = EnumMapping.asEnum;
-        settings.jsonLibrary = JsonLibrary.jaxb;
+        settings.jsonLibrary = JsonLibrary.jackson2;
         final ClassEnumExtension classEnumExtension = new ClassEnumExtension();
-        classEnumExtension.setConfiguration(Collections.singletonMap("classEnumPattern", "ClassEnum"));
+        classEnumExtension.setConfiguration(Collections.singletonMap("classEnumPattern", "Enum"));
         settings.extensions.add(classEnumExtension);
-        final String output = new TypeScriptGenerator(settings).generateTypeScript(Input.from(DummyClassEnum.class));
+        final String output = new TypeScriptGenerator(settings).generateTypeScript(Input.from(DummyEnum.class, DummyClassEnum.class));
         final String expected = (
-            "\ndeclare const enum DummyClassEnum {\n" +
-            "    ATYPE = 'ATYPE',\n" +
-            "    BTYPE = 'BTYPE',\n" +
-            "    CTYPE = 'CTYPE',\n" +
-            "}\n"
-        ).replace("'", "\"");
+                "\ndeclare const enum DummyClassEnum {\n" +
+                        "    ATYPE = 'ATYPE',\n" +
+                        "    BTYPE = 'BTYPE',\n" +
+                        "    CTYPE = 'CTYPE',\n" +
+                        "}\n" +
+                "\ndeclare const enum DummyEnum {\n" +
+                        "    Red = 'Red',\n" +
+                        "    Green = 'Green',\n" +
+                        "    Blue = 'Blue',\n" +
+                        "}\n"
+                ).replace("'", "\"");
         assertEquals(expected.trim(), output.trim());
     }
 
