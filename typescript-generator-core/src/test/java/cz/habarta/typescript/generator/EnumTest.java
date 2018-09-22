fb90c4aab3a26e78e61f159433d3e87d60f816c8
Jeramy Toplak <jjamestoplak@gmail.com>
2018-09-22 08:21:06 -0600

    IntelliJ auto-import breaks AppVeyor check style :(

--- typescript-generator-core/src/test/java/cz/habarta/typescript/generator/EnumTest.java
+++ typescript-generator-core/src/test/java/cz/habarta/typescript/generator/EnumTest.java
@@ -7,14 +7,8 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import cz.habarta.typescript.generator.ext.ClassEnumExtension;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
+import java.util.*;
+import static org.junit.Assert.*;
 
 public class EnumTest {
 
