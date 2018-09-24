d59726d2bc01ea7b4481e28e5ab48cf83842806d
philba <p.bayer@freemail.hu>
2018-09-24 12:31:00 +0200

    #269 async support Ignoring @Suspended param

--- typescript-generator-core/src/main/java/cz/habarta/typescript/generator/parser/JaxrsApplicationParser.java
+++ typescript-generator-core/src/main/java/cz/habarta/typescript/generator/parser/JaxrsApplicationParser.java
@@ -22,6 +22,7 @@ import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.GenericEntity;
@@ -31,39 +32,41 @@ import javax.ws.rs.core.StreamingOutput;
 
 
 public class JaxrsApplicationParser {
-
+    
     private final Settings settings;
     private final Predicate<String> isClassNameExcluded;
     private final Set<String> defaultExcludes;
     private final JaxrsApplicationModel model;
-
+    
     public JaxrsApplicationParser(Settings settings) {
         this.settings = settings;
         this.isClassNameExcluded = settings.getExcludeFilter();
         this.defaultExcludes = new LinkedHashSet<>(getDefaultExcludedClassNames());
         this.model = new JaxrsApplicationModel();
     }
-
+    
     public JaxrsApplicationModel getModel() {
         return model;
     }
-
+    
     public static class Result {
         public List<SourceType<Type>> discoveredTypes;
+        
         public Result() {
             discoveredTypes = new ArrayList<>();
         }
+        
         public Result(List<SourceType<Type>> discoveredTypes) {
             this.discoveredTypes = discoveredTypes;
         }
     }
-
+    
     public Result tryParse(SourceType<?> sourceType) {
         if (!(sourceType.type instanceof Class<?>)) {
             return null;
         }
         final Class<?> cls = (Class<?>) sourceType.type;
-
+        
         // application
         if (Application.class.isAssignableFrom(cls)) {
             final ApplicationPath applicationPathAnnotation = cls.getAnnotation(ApplicationPath.class);
@@ -74,7 +77,7 @@ public class JaxrsApplicationParser {
             final List<SourceType<Type>> discoveredTypes = JaxrsApplicationScanner.scanJaxrsApplication(cls, isClassNameExcluded);
             return new Result(discoveredTypes);
         }
-
+        
         // resource
         final Path path = cls.getAnnotation(Path.class);
         if (path != null) {
@@ -83,10 +86,10 @@ public class JaxrsApplicationParser {
             parseResource(result, new ResourceContext(cls, path.value()), cls);
             return result;
         }
-
+        
         return null;
     }
-
+    
     private void parseResource(Result result, ResourceContext context, Class<?> resourceClass) {
         // subContext
         final Map<String, Type> pathParamTypes = new LinkedHashMap<>();
@@ -117,7 +120,7 @@ public class JaxrsApplicationParser {
             parseResourceMethod(result, subContext, resourceClass, method);
         }
     }
-
+    
     private void parseResourceMethod(Result result, ResourceContext context, Class<?> resourceClass, Method method) {
         final Path pathAnnotation = method.getAnnotation(Path.class);
         // subContext
@@ -202,13 +205,13 @@ public class JaxrsApplicationParser {
             parseResource(result, context, method.getReturnType());
         }
     }
-
+    
     private void foundType(Result result, Type type, Class<?> usedInClass, String usedInMember) {
         if (!isExcluded(type)) {
             result.discoveredTypes.add(new SourceType<>(type, usedInClass, usedInMember));
         }
     }
-
+    
     private boolean isExcluded(Type type) {
         final Class<?> cls = Utils.getRawClassOrNull(type);
         if (cls == null) {
@@ -220,7 +223,7 @@ public class JaxrsApplicationParser {
         if (defaultExcludes.contains(cls.getName())) {
             return true;
         }
-
+        
         for (Class<?> standardEntityClass : getStandardEntityClasses()) {
             if (standardEntityClass.isAssignableFrom(cls)) {
                 return true;
@@ -228,7 +231,7 @@ public class JaxrsApplicationParser {
         }
         return false;
     }
-
+    
     private static HttpMethod getHttpMethod(Method method) {
         for (Annotation annotation : method.getAnnotations()) {
             final HttpMethod httpMethodAnnotation = annotation.annotationType().getAnnotation(HttpMethod.class);
@@ -238,7 +241,7 @@ public class JaxrsApplicationParser {
         }
         return null;
     }
-
+    
     private static MethodParameterModel getEntityParameter(Method method) {
         for (Parameter parameter : method.getParameters()) {
             if (!hasAnyAnnotation(parameter, Arrays.asList(
@@ -247,15 +250,16 @@ public class JaxrsApplicationParser {
                     PathParam.class,
                     CookieParam.class,
                     HeaderParam.class,
+                    Suspended.class,
                     Context.class,
                     FormParam.class
-                    ))) {
+            ))) {
                 return new MethodParameterModel(parameter.getName(), parameter.getParameterizedType());
             }
         }
         return null;
     }
-
+    
     private static boolean hasAnyAnnotation(Parameter parameter, List<Class<? extends Annotation>> annotationClasses) {
         for (Class<? extends Annotation> annotationClass : annotationClasses) {
             for (Annotation parameterAnnotation : parameter.getAnnotations()) {
@@ -266,7 +270,7 @@ public class JaxrsApplicationParser {
         }
         return false;
     }
-
+    
     public static List<Class<?>> getStandardEntityClasses() {
         // JAX-RS specification - 4.2.4 Standard Entity Providers
         return Arrays.asList(
@@ -283,33 +287,33 @@ public class JaxrsApplicationParser {
                 java.lang.Boolean.class, java.lang.Character.class, java.lang.Number.class,
                 long.class, int.class, short.class, byte.class, double.class, float.class, boolean.class, char.class);
     }
-
+    
     private static List<String> getDefaultExcludedClassNames() {
         return Arrays.asList(
                 "org.glassfish.jersey.media.multipart.FormDataBodyPart"
         );
     }
-
+    
     private static class ResourceContext {
         public final Class<?> rootResource;
         public final String path;
         public final Map<String, Type> pathParamTypes;
-
+        
         public ResourceContext(Class<?> rootResource, String path) {
             this(rootResource, path, new LinkedHashMap<String, Type>());
         }
-
+        
         private ResourceContext(Class<?> rootResource, String path, Map<String, Type> pathParamTypes) {
             this.rootResource = rootResource;
             this.path = path;
             this.pathParamTypes = pathParamTypes;
         }
-
+        
         ResourceContext subPath(Path pathAnnotation) {
             final String subPath = pathAnnotation != null ? pathAnnotation.value() : null;
             return new ResourceContext(rootResource, Utils.joinPath(path, subPath), pathParamTypes);
         }
-
+        
         ResourceContext subPathParamTypes(Map<String, Type> subPathParamTypes) {
             final Map<String, Type> newPathParamTypes = new LinkedHashMap<>();
             newPathParamTypes.putAll(pathParamTypes);
@@ -319,5 +323,5 @@ public class JaxrsApplicationParser {
             return new ResourceContext(rootResource, path, newPathParamTypes);
         }
     }
-
+    
 }
