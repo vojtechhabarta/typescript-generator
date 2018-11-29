84ed1d9de64ac3c51e9fa50d4f1247ccfc8c64ff
Vojtěch Habarta <vojtech@habarta.cz>
2018-11-29 13:52:51 +0100

    Fix link to `Jackson2Configuration` class in javadoc

--- typescript-generator-maven-plugin/src/main/java/cz/habarta/typescript/generator/maven/GenerateMojo.java
+++ typescript-generator-maven-plugin/src/main/java/cz/habarta/typescript/generator/maven/GenerateMojo.java
@@ -167,7 +167,7 @@ public class GenerateMojo extends AbstractMojo {
     /**
      * Specifies Jackson 2 global configuration.
      * Description of individual parameters is in
-     * <a href="https://github.com/vojtechhabarta/typescript-generator/blob/master/typescript-generator-core/src/main/java/cz/habarta/typescript/generator/Settings.java">Jackson2Configuration</a>
+     * <a href="https://github.com/vojtechhabarta/typescript-generator/blob/master/typescript-generator-core/src/main/java/cz/habarta/typescript/generator/Jackson2Configuration.java">Jackson2Configuration</a>
      * class on GitHub (latest version).
      */
     @Parameter
