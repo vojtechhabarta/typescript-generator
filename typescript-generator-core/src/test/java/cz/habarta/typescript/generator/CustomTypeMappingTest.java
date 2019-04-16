add6b4c0a16d68afce6787b3197d98314a1d392a
Vojtěch Habarta <vojtech@habarta.cz>
2019-04-16 13:20:38 +0200

    Test for custom enum mapping as JSON object (#327)

--- typescript-generator-core/src/test/java/cz/habarta/typescript/generator/CustomTypeMappingTest.java
+++ typescript-generator-core/src/test/java/cz/habarta/typescript/generator/CustomTypeMappingTest.java
@@ -1,7 +1,14 @@
 
 package cz.habarta.typescript.generator;
 
-import java.util.*;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -27,6 +34,73 @@ public class CustomTypeMappingTest {
     private static class CustomTypesUsage {
         public Date date1;
         public Calendar calendar1;
+    }
+
+    @Test
+    public void testEnumAsMap() throws Exception {
+//        final ObjectMapper objectMapper = new ObjectMapper();
+//        final String json = objectMapper.writeValueAsString(MyEnum.MY_FIRST_VALUE);
+//        System.out.println(json);
+
+        final Settings settings = TestUtils.settings();
+        settings.customTypeMappings = Collections.singletonMap("cz.habarta.typescript.generator.CustomTypeMappingTest$MyEnum", "{ code: string, definition: string }");
+        final String output = new TypeScriptGenerator(settings).generateTypeScript(Input.from(MyInterfUsingEnum.class));
+        System.out.println(output);
+        assertTrue(output.contains("someValue: { code: string, definition: string }"));
+    }
+
+    @JsonSerialize(using = CodedValueSerializer.class)
+    public interface CodedValue {
+        String getCode();
+        String getDefinition();
+    }
+
+    public enum MyEnum implements CodedValue {
+
+        MY_FIRST_VALUE("A0", "Some description");
+
+        private final String code;
+        private final String definition;
+
+        private MyEnum(String code, String definition) {
+            this.code = code;
+            this.definition = definition;
+        }
+
+        @Override
+        public String getCode() {
+            return code;
+        }
+
+        @Override
+        public String getDefinition() {
+            return definition;
+        }
+    }
+
+    public interface MyInterfUsingEnum {
+        public MyEnum getSomeValue();
+    }
+
+    public static class CodedValueSerializer extends StdSerializer<CodedValue> {
+
+        private static final long serialVersionUID = 1L;
+
+        public CodedValueSerializer() {
+            super(CodedValue.class);
+        }
+
+        public CodedValueSerializer(Class<CodedValue> t) {
+            super(t);
+        }
+
+        @Override
+        public void serialize(CodedValue value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+            gen.writeStartObject();
+            gen.writeStringField("code", value.getCode());
+            gen.writeStringField("definition", value.getDefinition());
+            gen.writeEndObject();
+        }
     }
 
 }
