42ba2ee82f829489380bf434dee5cf2917eb0749
Jack Clinton <jack@opengamma.com>
2019-10-28 17:09:34 +0000

    Remove misplaced header

--- typescript-generator-core/src/main/java/cz/habarta/typescript/generator/CustomMappingScope.java
+++ typescript-generator-core/src/main/java/cz/habarta/typescript/generator/CustomMappingScope.java
@@ -1,8 +1,3 @@
-/*
- * Copyright (C) 2019 - present by OpenGamma Inc. and the OpenGamma group of companies
- *
- * Please see distribution for license.
- */
 package cz.habarta.typescript.generator;
 
 public enum CustomMappingScope {
