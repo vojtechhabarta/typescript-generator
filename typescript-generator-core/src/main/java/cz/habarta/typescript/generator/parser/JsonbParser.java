e788b346b971c461a25c1674b1dccf3bf4ebaa85
Romain Manni-Bucau <rmannibucau@gmail.com>
2020-04-16 09:11:43 +0200

    enums must be processed, even when comming from a dependency

--- typescript-generator-core/src/main/java/cz/habarta/typescript/generator/parser/JsonbParser.java
+++ typescript-generator-core/src/main/java/cz/habarta/typescript/generator/parser/JsonbParser.java
@@ -102,6 +102,9 @@ public class JsonbParser extends ModelParser {
         for (Type aInterface : interfaces) {
             addBeanToQueue(new SourceType<>(aInterface, sourceClass.type, "<interface>"));
         }
+        properties.stream()
+                .filter(p -> Class.class.isInstance(p.getType()) && Class.class.cast(p.getType()).isEnum())
+                .forEach(enumProp -> addBeanToQueue(new SourceType<>(Class.class.cast(enumProp.getType()))));
         return new BeanModel(
                 sourceClass.type, superclass, null, null, null,
                 interfaces, properties, null);
