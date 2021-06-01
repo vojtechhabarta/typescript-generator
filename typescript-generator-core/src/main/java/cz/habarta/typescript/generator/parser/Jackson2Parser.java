06b881e5f49b4d8487cdc299645023265778a402
Vojtěch Habarta <vojtech@habarta.cz>
2021-06-01 20:20:55 +0200

    Improve exception handling when getting enum constants (#677)

--- typescript-generator-core/src/main/java/cz/habarta/typescript/generator/parser/Jackson2Parser.java
+++ typescript-generator-core/src/main/java/cz/habarta/typescript/generator/parser/Jackson2Parser.java
@@ -719,12 +719,13 @@ public class Jackson2Parser extends ModelParser {
 
         final List<EnumMemberModel> enumMembers = new ArrayList<>();
         if (sourceClass.type.isEnum()) {
+            @SuppressWarnings("unchecked")
             final Class<Enum> enumClass = (Class<Enum>) sourceClass.type;
             final List<Field> constants = Arrays.stream(enumClass.getEnumConstants()).map(e -> {
                 try {
                     return enumClass.getDeclaredField(e.name());
                 } catch (NoSuchFieldException noSuchFieldException) {
-                    return null;
+                    throw new RuntimeException(noSuchFieldException);
                 }
             }).collect(Collectors.toList());
             for (Field constant : constants) {
