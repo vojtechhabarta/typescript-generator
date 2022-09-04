0036a9bab83d0ec1023c99ece0006b06d18ea728
Vojtěch Habarta <vojtech@habarta.cz>
2022-09-04 23:53:58 +0200

    GitHub Action for releasing to Maven Central repository

--- build/release-build.ps1
+++ build/release-build.ps1
@@ -1,4 +1,15 @@
 
+# Releases latest build to Maven Central Repository
+# Prerequisites:
+# - gpg with signing key without passphrase
+# - OSSRH_USERNAME
+# - OSSRH_PASSWORD
+
+if (-not $env:OSSRH_USERNAME -or -not $env:OSSRH_PASSWORD) {
+    Write-Error "Please set OSSRH_USERNAME and OSSRH_PASSWORD environment variables"
+    exit 1
+}
+
 $ErrorActionPreference = "Stop"
 
 # download
@@ -14,40 +25,27 @@ $unzipDirectory = New-Item -ItemType directory -Path $unzipDirectoryPath -Force
 [System.IO.Compression.ZipFile]::ExtractToDirectory($zipFile, $unzipDirectory.FullName)
 $basePath = (Resolve-Path $unzipDirectory).Path
 
-# passphrase
-$securePassphrase = Read-Host -Prompt "Enter signing key passphrase" -AsSecureString
-$passphraseCredential = New-Object System.Management.Automation.PSCredential -ArgumentList "Domain\User", $securePassphrase
-$passphrase = $passphraseCredential.GetNetworkCredential().Password
-# test passphrase
-gpg --detach-sign --armor --passphrase $passphrase $zipFile
-if (! $?) {
-    exit
-}
-
 # sign
-foreach ($file in dir -Recurse -File $basePath -Exclude *.md5,*.sha1) {
+foreach ($file in Get-ChildItem -Recurse -File $basePath -Exclude *.md5,*.sha1) {
     $path = $file.FullName.Substring($basePath.Length + 1)
     Write-Host -ForegroundColor DarkCyan "Signing $path..."
-    gpg --detach-sign --armor --passphrase $passphrase $file.FullName
-    Get-FileHash -Algorithm MD5 $file.FullName | % { [IO.File]::WriteAllText($file.FullName + ".md5", $_.Hash) }
-    Get-FileHash -Algorithm SHA1 $file.FullName | % { [IO.File]::WriteAllText($file.FullName + ".sha1", $_.Hash) }
+    gpg --detach-sign --armor $file.FullName
+    Get-FileHash -Algorithm MD5 $file.FullName | ForEach-Object { [IO.File]::WriteAllText($file.FullName + ".md5", $_.Hash) }
+    Get-FileHash -Algorithm SHA1 $file.FullName | ForEach-Object { [IO.File]::WriteAllText($file.FullName + ".sha1", $_.Hash) }
 }
 
 # upload
 $repoUri = "https://oss.sonatype.org/service/local/staging/deploy/maven2"
-$credential = $host.UI.PromptForCredential("OSS Repository Hosting", "Enter your credentials for Sonatype maven repository https://oss.sonatype.org", "", "")
-if (! $credential) {
-    exit
-}
-$headers = @{"Authorization" = "Basic " + [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($credential.UserName + ":" + $credential.GetNetworkCredential().Password))}
-foreach ($file in dir -Recurse -File $basePath) {
+$credential = New-Object -TypeName System.Net.NetworkCredential -ArgumentList $env:OSSRH_USERNAME, $env:OSSRH_PASSWORD
+$headers = @{"Authorization" = "Basic " + [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($credential.UserName + ":" + $credential.Password))}
+foreach ($file in Get-ChildItem -Recurse -File $basePath) {
     $path = $file.FullName.Substring($basePath.Length + 1)
     Write-Host -ForegroundColor DarkCyan "Uploading $path..."
-    $pom = (dir $file.Directory -Filter *.pom).FullName
+    $pom = (Get-ChildItem $file.Directory -Filter *.pom).FullName
     [xml]$pomXml = Get-Content $pom
     $groupId = if ($pomXml.project.groupId) { $pomXml.project.groupId } else { $pomXml.project.parent.groupId }
     $uri = $groupId.Replace(".", "/") + $path.Substring($groupId.Length).Replace("\", "/")
-    $response = Invoke-WebRequest -InFile $file.FullName -Method Put "$repoUri/$uri" -Headers $headers -UseBasicParsing
+    Invoke-WebRequest -InFile $file.FullName -Method Put "$repoUri/$uri" -Headers $headers -UseBasicParsing | Out-Null
 }
 
 Write-Host -ForegroundColor Cyan "Build successfully uploaded. Go to https://oss.sonatype.org and promote the release."
