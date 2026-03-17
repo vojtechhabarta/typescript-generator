423ed46ae58563ab259382be2e3483a009c1bb4b
Vojtěch Habarta <vojtech@habarta.cz>
2026-03-17 22:37:58 +0100

    Test for Java `record`, moved test for parent exclusion (#1097)

---
+++ typescript-generator-core/src/test/java/cz/habarta/typescript/generator/ParentClassExclusionTest.java
@@ -0,0 +1,31 @@
+
+package cz.habarta.typescript.generator;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class ParentClassExclusionTest {
+
+    private static class Base {
+    }
+
+    private static class Derived extends Base {
+        public String name;
+    }
+
+    @Test
+    public void testParentClassExclusion() {
+        final Settings settings = TestUtils.settings();
+        settings.outputFileType = TypeScriptFileType.implementationFile;
+        settings.mapClasses = ClassMapping.asClasses;
+        settings.generateConstructors = true;
+        settings.setExcludeFilter(List.of(Base.class.getName()), null);
+        final String output = new TypeScriptGenerator(settings).generateTypeScript(Input.from(Derived.class));
+        Assertions.assertTrue(!output.contains("super"));
+        Assertions.assertTrue(!output.contains("extends"));
+        Assertions.assertTrue(!output.contains("implements"));
+    }
+
+}
--- typescript-generator-core/src/test/java/cz/habarta/typescript/generator/RecordTest.java
+++ typescript-generator-core/src/test/java/cz/habarta/typescript/generator/RecordTest.java
@@ -1,49 +1,33 @@
 
 package cz.habarta.typescript.generator;
 
-import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
 public class RecordTest {
 
-    // public record Account(Long id, String name) {}
+    public record Account(Long id, String name) {}
 
-    // @Test
-    // public void test() {
-    // final Settings settings = TestUtils.settings();
-    // final String output = new TypeScriptGenerator(settings).generateTypeScript(Input.from(Account.class));
-    // Assertions.assertTrue(!output.contains("interface Record"));
-    // }
-
-    // @Test
-    // public void testConstructor() {
-    // final Settings settings = TestUtils.settings();
-    // settings.outputFileType = TypeScriptFileType.implementationFile;
-    // settings.mapClasses = ClassMapping.asClasses;
-    // settings.generateConstructors = true;
-    // final String output = new TypeScriptGenerator(settings).generateTypeScript(Input.from(Account.class));
-    // System.out.println(output);
-    // Assertions.assertTrue(!output.contains("interface Record"));
-    // }
-
-    private static class Base {
-    }
-
-    private static class Derived extends Base {
-        public String name;
+    @Test
+    public void test() {
+        final Settings settings = TestUtils.settings();
+        final String output = new TypeScriptGenerator(settings).generateTypeScript(Input.from(Account.class));
+        Assertions.assertTrue(!output.contains("interface Record"));
+        Assertions.assertTrue(output.contains("id: number"));
+        Assertions.assertTrue(output.contains("name: string"));
     }
 
     @Test
-    public void testConstructorWithExclusion() {
+    public void testConstructor() {
         final Settings settings = TestUtils.settings();
         settings.outputFileType = TypeScriptFileType.implementationFile;
         settings.mapClasses = ClassMapping.asClasses;
         settings.generateConstructors = true;
-        settings.setExcludeFilter(List.of(Base.class.getName()), null);
-        final String output = new TypeScriptGenerator(settings).generateTypeScript(Input.from(Derived.class));
+        final String output = new TypeScriptGenerator(settings).generateTypeScript(Input.from(Account.class));
         Assertions.assertTrue(!output.contains("interface Record"));
+        Assertions.assertTrue(!output.contains("super"));
+        Assertions.assertTrue(output.contains("constructor(data: Account)"));
     }
 
 }
